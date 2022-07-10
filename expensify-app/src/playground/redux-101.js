44e62b9f9515a7e2a7c4d5854ded1eb3e396a8d6
Skoomar <umar_farooqi@hotmail.co.uk>
2022-07-11 00:53:34 +0100

    Sec10 chp90

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,9 +1,43 @@
 import {createStore} from "@reduxjs/toolkit";
 
+
+// const add = (data) => {
+//     return data.a + data.b
+// }
+
+// rewrite the above add function but use destruction to make it look nicer
+// so instead of taking the 'data' array as parameter and then having to use it like indexes. Just put { x... } in the parameters and it'll deal
+// works for objects and arrays
+const add = ({ a, b }) => {
+    return a + b;
+}
+
+// Action Generators
+
+// a function to be used as an Action Generator
 // shorthand arrow function syntax rememberrrrr
-const incrementCount = () => ({
-    type: 'INCREMENT'
+// payload is anything passed into the function. is empty by default but we can use to pass stuff like incrementBy etc
+// ok ignore the payload thing, we've changed that to use the destruct stuff to put incrementBy directly there. default value for incrementBy set to 1
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    // incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1 -- don't need this anymore as our destruction stuff above handles the case if incrementBy isn't passed in
+    // also remember we don't have to write out incrementBy: incrementBy as they have the same name. Can just put incrementBy and it'll know what we mean
+    incrementBy
 });
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count }) => ({
+    type: 'SET',
+    count
+})
+
+const resetCount = () => ({
+    type: 'RESET',
+})
 
 // createStore takes state and actions
 // here we've also passed a default value for state (setting count to 0)
@@ -11,9 +45,9 @@ const store = createStore((state = {count: 0}, action) => {
     // it's convention to use switch statements in createStore to deal with the actions
     switch (action.type) {
         case 'INCREMENT':
-            // either take the incrementBy value passed in from the dispath() call or default to incrementing by 1
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-            return {count: state.count + incrementBy};
+            // either take the incrementBy value passed in from the dispatch() call or default to incrementing by 1 ---
+            // const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1; - no longer need this here as we've put it in the action generator above
+            return {count: state.count + action.incrementBy};
         case 'DECREMENT':
             const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {count: state.count - decrementBy};
@@ -39,27 +73,21 @@ store.dispatch({
     incrementBy: 5
 });
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+// write dispatch as an 'Action Generator' instead of having to write out the action each time
+store.dispatch(incrementCount());
+
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
 // call the decrement action
-store.dispatch({
-    type: 'DECREMENT'
-});
+// store.dispatch({
+//     type: 'DECREMENT'
+// });
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 9
-});
+store.dispatch(decrementCount({ decrementBy: 12 }))
 
-store.dispatch({
-    type: 'SET',
-    count: 0
-});
-store.dispatch({
-    type: 'RESET'
-})
+store.dispatch(setCount({ count: 50 }));
+
+store.dispatch(resetCount());
 
 // cancel the subscriptiono
 unsubscribe();
